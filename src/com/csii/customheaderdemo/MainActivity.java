7687eb8b1a0c2da6b627ef4657426550266085ea
yzj <yanzhanji130@163.com>
2016-05-15 09:18:51 +0800

    1.修改1

--- src/com/csii/customheaderdemo/MainActivity.java
+++ src/com/csii/customheaderdemo/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener {
         customHeaderView.setmHeaderBitmap(bgBitmap1);
     }
 	private void initSeekBar() {
-		seekBar1.setMax(1000);
+		seekBar1.setMax(200);
 		seekBar2.setMax(mWidth/2);
 		seekBar3.setMax(mWidth);
 		
